1ae12d84d17427bb8ffb172d39a5454fdcd03fa3
nekoshock <279801449@qq.com>
2019-04-09 10:22:39 +0800

    2019-4-9 10:22:37

--- JAVA/chapter01/src/chapter01/HelloWorld.java
+++ JAVA/chapter01/src/chapter01/HelloWorld.java
@@ -3,5 +3,17 @@ package chapter01;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
+		Sub1 ob = new Super();
+		System.out.println(ob.a);
+		System.out.println(ob.b);
 	}
+}
+
+class Sub1 {
+	String a = "sub";
+	String b = "top";
+}
+class Super extends Sub1 {
+	String a = "super";
+	String b = "super2";
 }
